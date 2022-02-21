feeb2ee94f8afa31bdf5608a6cd390c25cac93f3
iyoman <42587833+iyoman@users.noreply.github.com>
2022-02-21 17:43:48 -0500

    Update unitybezier.cs

--- unitybezier.cs
+++ unitybezier.cs
@@ -1,30 +1,20 @@
 private const float directionScale = 0.5f;
-	
+
 	private void OnSceneGUI () {
 		curve = target as BezierCurve;
 		handleTransform = curve.transform;
 		handleRotation = Tools.pivotRotation == PivotRotation.Local ?
 			handleTransform.rotation : Quaternion.identity;
-		
+
 		Vector3 p0 = ShowPoint(0);
 		Vector3 p1 = ShowPoint(1);
 		Vector3 p2 = ShowPoint(2);
-		Vector3 p3 = ShowPoint(3);
-		
+		Vector2 p3 = ShowPoint(3);
+
 		Handles.color = Color.gray;
 		Handles.DrawLine(p0, p1);
 		Handles.DrawLine(p2, p3);
-		
+
 		ShowDirections();
 		Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 2f);
-	}
-
-	private void ShowDirections () {
-		Handles.color = Color.green;
-		Vector3 point = curve.GetPoint(0f);
-		Handles.DrawLine(point, point + curve.GetDirection(0f) * directionScale);
-		for (int i = 1; i <= lineSteps; i++) {
-			point = curve.GetPoint(i / (float)lineSteps);
-			Handles.DrawLine(point, point + curve.GetDirection(i / (float)lineSteps) * directionScale);
-		}
 	}
