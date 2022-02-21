845baf02dc5013d4ed77024f27345a4b4d9128d9
iyoman <42587833+iyoman@users.noreply.github.com>
2022-02-21 17:37:55 -0500

    Update unitybezier.cs

--- unitybezier.cs
+++ unitybezier.cs
@@ -1,21 +1,20 @@
 private const float directionScale = 0.5f;
-
+	
 	private void OnSceneGUI () {
 		curve = target as BezierCurve;
 		handleTransform = curve.transform;
 		handleRotation = Tools.pivotRotation == PivotRotation.Local ?
 			handleTransform.rotation : Quaternion.identity;
-
+		
 		Vector3 p0 = ShowPoint(0);
 		Vector3 p1 = ShowPoint(1);
 		Vector3 p2 = ShowPoint(2);
-		Vector2 p3 = ShowPoint(3);
-
-		//not sure where handles comes from, might just be a special editor var
+		Vector3 p3 = ShowPoint(3);
+		
 		Handles.color = Color.gray;
 		Handles.DrawLine(p0, p1);
 		Handles.DrawLine(p2, p3);
-
+		
 		ShowDirections();
 		Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 2f);
 	}
